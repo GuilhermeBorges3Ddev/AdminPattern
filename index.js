81f132df59238c8e1914a214a6f6eef5f4d8c7b8
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-01-04 17:19:11 -0300

    Returning in console all informations about the fields

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ var admin = document.querySelector("#exampleInputAdmin");
 var fields = document.querySelectorAll("#form-user-create [name]");
 
 fields.forEach(function(field, index){
-    console.log(field.name);
+    console.log(field.id, field.name, field.value, field.checked, index);
 });
