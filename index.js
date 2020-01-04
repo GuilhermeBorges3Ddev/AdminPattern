056d5a36079390c017ee51b5d362d5b601e50a59
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-01-04 20:22:05 -0300

    addLine() function replace the list updated of users into the HTML display

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ var fields = document.querySelectorAll("#form-user-create [name]");
 //Variable used outside the forEach, because it'll be written like a JSON after...
 var user = {};
 
-//Using conditions in if-else to put filter into the array called fields
-
-
-//Testing the return of json
-console.log(user);
+//Simple function who adds one more data row inside our HTML table
+function addLine(dataUser){
+    var tr = document.createElement("tr");
+    tr.innerHTML = `
+        <tr>
+            <td>
+                <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
+            </td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.data}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+    `;
+    document.getElementById("table-users").appendChild(tr);
+}
 
 //When form send button is clicked the refresh default event is disabled
 document.getElementById("form-user-create").addEventListener("submit", function(event){
@@ -22,6 +37,6 @@ document.getElementById("form-user-create").addEventListener("submit", function(
             user[field.name] = field.value;
         }
     });
-    //Get the filled form into the JSON
-    console.log(fields);
+    //
+    addLine(user);
 });
