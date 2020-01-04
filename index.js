8b4369f8a38be552d52344b6540aeced65d595fc
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-01-04 15:09:56 -0300

    All form itens of admin template are saved inside variables

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-alert("JS incorporatted");
+
+//Getting form itens and using variables to set some values and styles
+
+var nome = document.querySelector("#exampleInputName");
+nome.value = "Guilherme";
+nome.style.color = "blue";
+
+var gender = document.querySelector("#exampleInputGenderM");
+var dateBirth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var mail = document.querySelector("#exampleInputEmail1");
+var password = document.querySelector("#exampleInputPassword1")
