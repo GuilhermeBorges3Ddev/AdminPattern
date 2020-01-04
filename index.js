ce1e79a3edb669ff22b6281c4b2b8c5f3f65d020
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-01-04 19:05:25 -0300

    Filled form not refresh the page and it's converted into a JSON

--- index.js
+++ index.js
@@ -5,15 +5,23 @@ var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
 //Using conditions in if-else to put filter into the array called fields
-fields.forEach(function(field, index){
-    if(field.name == "gender"){
-        if(field.checked){
-            user[field.name] = field.value;
-        }
-    }else{
-        user[field.name] = field.value;
-    }
-});
+
 
 //Testing the return of json
 console.log(user);
+
+//When form send button is clicked the refresh default event is disabled
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+    event.preventDefault();
+    fields.forEach(function(field, index){
+        if(field.name == "gender"){
+            if(field.checked){
+                user[field.name] = field.value;
+            }
+        }else{
+            user[field.name] = field.value;
+        }
+    });
+    //Get the filled form into the JSON
+    console.log(fields);
+});
